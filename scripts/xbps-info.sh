d034048eaf6592e0cfc6cc082c08c46e48a075d5
Priyanshu <normalpriyanshu@gmail.com>
2025-10-03 23:46:03 +0530

    fix: xbps-info.sh

--- scripts/xbps-info.sh
+++ scripts/xbps-info.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pkgname=$(echo $PWD | sed 's/\///g')
-xq $pkgname
+pkgname=$(echo $PWD | sed 's/\//_/g')
+XBPS_HOSTDIR=~/.local/share/virt-pack/xbps/hostdir xq $pkgname
--- scripts/xbps-resolve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "resolving libs, in case of multiple choose one"
-
-for arg in "$@"; do
-    xlocate $arg.pc | fzf -1 | cut -d' ' -f1
-done
